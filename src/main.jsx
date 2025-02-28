568a5fd607c22fb05ceae07e7e8560d738c9944e
Suman De <sumande414@gmail.com>
2025-02-28 22:24:54 +0530

    bug fixes

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter } from "react-router";
 import App from './App.jsx'
-import AppRoutes from "./config/routes.jsx";
+import AppRoutes from "./config/Routes.jsx";
 import { Toaster } from "react-hot-toast";
 import { ChatProvider } from "./context/ChatContext.jsx";
 
