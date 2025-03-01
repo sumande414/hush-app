761a89754753cfa374b1e82c0a9b6c585f0ad395
Suman De <sumande414@gmail.com>
2025-03-01 09:38:44 +0530

    test

--- src/config/helper.js
+++ src/config/helper.js
@@ -1,7 +1,9 @@
 export function timeAgo(date) {
-  const now = new Date(Date.now());
+  const now = new Date();
   const past = new Date(date);
   const secondsAgo = Math.floor((now - past) / 1000);
+  console.log(now);
+  console.log(past);
 
   if (secondsAgo < 60) return `${secondsAgo} seconds ago`;
   const minutesAgo = Math.floor(secondsAgo / 60);
